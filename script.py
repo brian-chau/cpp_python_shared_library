1309529dd6baaef0906f2c5d396e3f9a143a0685
Brian Chau <brian.chau@outlook.com>
2021-11-20 23:38:12 -0800

    Rebuild using Lubuntu VM

--- script.py
+++ script.py
@@ -3,14 +3,14 @@
 from ctypes import *
 
 def main():
-    lib = cdll.LoadLibrary('/home/brian/windows/brian/Desktop/cpp_shared_object/exe.so')
-    
+    lib = cdll.LoadLibrary('/home/user/Desktop/cpp_python_shared_library/exe.so')
+
     func          = lib.count_PU_DO_Locations
     func.argtypes = [c_char_p, POINTER(c_int), POINTER(c_int)]
 
     pu_count, do_count = c_int(), c_int()
 
-    func(b'/home/brian/windows/brian/Desktop/cpp_shared_object/yellowcab.bin', pu_count, do_count)
+    func(b'/home/user/Desktop/cpp_python_shared_library/out.bin', pu_count, do_count)
     print(pu_count.value)
     print(do_count.value)
 
